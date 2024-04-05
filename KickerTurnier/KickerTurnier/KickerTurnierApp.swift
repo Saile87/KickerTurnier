6a3f6cf686f023a02034001c78bb07707e222833
Elias Breitenbach <elias.b@me.com>
2024-04-05 20:33:58 +0200

    Create a project on github

---
+++ KickerTurnier/KickerTurnier/KickerTurnierApp.swift
@@ -0,0 +1,17 @@
+//
+//  KickerTurnierApp.swift
+//  KickerTurnier
+//
+//  Created by Elias Breitenbach on 04.04.24.
+//
+
+import SwiftUI
+
+@main
+struct KickerTurnierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
