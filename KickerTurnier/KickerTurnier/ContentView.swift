28ef855794f462e41a9be653092612a2d1a23bdf
Elias Breitenbach <elias.b@me.com>
2024-04-07 10:25:11 +0200

    Create a Button

--- KickerTurnier/KickerTurnier/ContentView.swift
+++ KickerTurnier/KickerTurnier/ContentView.swift
@@ -8,14 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showAlert = false
+    @State private var turnierListen: [String] = []
+    @State private var newTextInput = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            List {
+                ForEach(turnierListen, id: \.self) { turnierListe in
+                    Text(turnierListe)
+                }
+            }
+            
+                Spacer()
+                Button("Turnier Name hinzufügen") {
+                    showAlert = true
+                }
+                .font(.title3)
+                
+                .alert("Bitte geben Sie einen Namen für das neue Turnier ein:", isPresented: $showAlert, actions: {
+                    TextField("TextField", text: $newTextInput)
+                    
+                    Button("Abbrechen", role: .cancel, action: {})
+                    Button("Erstellen", action: {
+                        self.turnierListen.append(self.newTextInput)
+                        self.newTextInput = ""
+                    })
+                })
         }
-        .padding()
     }
 }
 
